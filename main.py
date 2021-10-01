437d8bcd0272a3f43646c48ce23672e518b83fe5
alpersahin21 <91716275+alpersahin21@users.noreply.github.com>
2021-10-01 06:21:34 +0000

    alper sahin odev

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+x = int(input("Enter number x: "))
+
+y = int(input("Enter number y: "))
+
+z = x**y
+
+print("x**y =",z)
+
+logx = np.log2(x)
+
+print("log(x) = ",logx)
+
+print("80417")
+
